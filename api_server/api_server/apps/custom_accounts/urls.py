2d5c0489426c8883e471a02c8f8c7f5d844ee66a
leouno <leouno12@gmail.com>
2022-01-31 08:47:36 +0900

    feat: no need to use format_suffix_patterns

--- api_server/api_server/apps/custom_accounts/urls.py
+++ api_server/api_server/apps/custom_accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from api_server.apps.custom_accounts import views
 from api_server.apps.custom_accounts.apps import CustomAccountsConfig
 
 app_name = CustomAccountsConfig.name
 
-urlpatterns = format_suffix_patterns([
+urlpatterns = [
     path('hello/', views.HelloView.as_view(), name='hello'),
-])
+]
